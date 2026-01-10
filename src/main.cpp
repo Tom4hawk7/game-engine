d30f49559e9b62c7bce6a55f1daba8bc39c80e40
Thomas Mcgrath <115906617+Tom4hawk7@users.noreply.github.com>
2026-01-10 18:24:49 +1100

    Basic Opengl implementation

--- src/main.cpp
+++ src/main.cpp
@@ -1,8 +1,68 @@
 #include <iostream>
 
+#include <GL/glew.h>
+#include <GLFW/glfw3.h>
+
+using namespace std;
+
+
+void errorCallback(int error, const char *description)
+{
+    cerr << "GLFW Error " << error << ": " << description << endl;
+}
+
 int main()
 {
-    std::cout << "Hello world" << std::endl;
+    if (!glfwInit()) 
+    {
+        cerr << "failed to initialise GLFW" << endl;
+        return -1;
+    }
 
+    // Set GLFW callbacks
+    glfwSetErrorCallback(errorCallback);
+
+    // Set OpenGL version and profile
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 4);
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 1);
+    glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+    glfwWindowHint(GLFW_OPENGL_FORWARD_COMPAT, GL_TRUE);
+ 
+    // Create window
+    GLFWwindow *window = glfwCreateWindow(1200, 1200, "Game Engine", nullptr, nullptr);
+    if (!window) 
+    {
+        cerr << "Failed to create a GLFW window" << endl;
+        glfwTerminate();
+        return -1;
+    }
+
+    // Make OpenGL context current
+    glfwMakeContextCurrent(window);
+
+    // Initialise GLEW
+    glewExperimental = GL_TRUE;
+    if (glewInit() != GLEW_OK)
+    {
+        cerr << "Failed to initialise GLEW" << endl;
+        glfwDestroyWindow(window);
+        glfwTerminate();
+        return -1;
+    }
+
+    // Main loop
+    while (!glfwWindowShouldClose(window))
+    {
+        // Clear colour buffer
+        glClear(GL_COLOR_BUFFER_BIT);
+
+        // Swap buffers and poll events
+        glfwSwapBuffers(window);
+        glfwPollEvents();
+    }
+
+    // Cleanup
+    glfwDestroyWindow(window);
+    glfwTerminate();
     return 0;
 }
